ee125f375b856e46d259545dc75a3ff2cc43cfe8
Mike <mike-maike.fr@etu.u-paris.fr>
2026-09-02 09:44:35 +0200

    changed Residu to Protein

--- scripts/classes/Atom.py
+++ scripts/classes/Atom.py
@@ -2,7 +2,10 @@ import numpy as np
 
 class Atom:
 
-    def __init__(self, atom_num, atom_name, coords):
+    def __init__(self, atom_num, atom_name, resname, resnum, coords):
         self.atom_num = atom_num
         self.atom_name = atom_name
+        self.resname = resname
+        self.resnum = resnum
         self.coords = coords
+        self.aromatic = resname in ["HIS", "TRP", "TYR", "PHE"]
--- scripts/classes/Protein.py
+++ scripts/classes/Protein.py
@@ -1,16 +1,15 @@
 import classes.Atom 
 
-class Residu:
-    count = 0
+class Protein:
 
-    def __init__(self, resname, resnum, atoms):
-        self.resname = resname
-        self.resnum = resnum
-        self.aromatic = resname in ["HIS", "TRP", "TYR", "PHE"]
+    def __init__(self, prot_name, atoms):
+        self.prot_name = prot_name
         self.atoms = self.generate_atoms(atoms)
-
+        
     def generate_atoms(self, atoms):
         return [classes.Atom (atom[6:11],
                               atom[12:16], 
+                              atom[17:20],
+                              atom[23:26], 
                               [float(atom[30:38]), float(atom[39:46]), float(atom[47:54])]) for atom in atoms]
 
