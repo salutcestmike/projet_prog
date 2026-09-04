33fdf0a84281079e4053fb736503cfc7fc0b6b00
Mike <mike-maike.fr@etu.u-paris.fr>
2026-09-04 10:20:51 +0200

    main factoring

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate projet_prog
 
 python3 -u ./scripts/main.py \
-    -i data/pdb/1MH1.pdb \
+    -i data/pdb/lysozyme_253L.pdb \
     -o output/surfaces \
     -w 3 \
     -n 92 \
--- scripts/main.py
+++ scripts/main.py
@@ -26,7 +26,7 @@ def getArgs():
 
     return {
         "input": input,
-        "output_file": output_folder,
+        "output_folder": output_folder,
         "num_workers": int(args.num_workers),
         "n_sphere_points": int(args.n_sphere_points),
         "display_molecule": args.display_molecule if input.is_file() else False
@@ -45,80 +45,106 @@ if __name__ == "__main__":
     print(f"Files to process:")
     [print(f"{file}\n") for file in files_to_process]
 
+    n_points = args.get("n_sphere_points", 92)
+    for file in files_to_process:
+        atoms = []
+        atoms_type = []
 
-    n_points = 92
-    pdb_file_test1 = Path("data/pdb/test.pdb")
-    pdb_file_test2 = Path("data/pdb/benzene.pdb")
-    pdb_file1 = Path("data/pdb/insulin_3I40.pdb")
-    pdb_file2 = Path("data/pdb/lysozyme_253L.pdb")
-    pdb_file3 = Path("data/pdb/lysozyme_1GWD.pdb")
-    pdb_file4 = Path("data/pdb/1MH1.pdb")
-
-    atoms = []
-    atoms_type = []
-
-    with open(pdb_file2, "r") as f:
-        for line in f:
-            if line.startswith(("ATOM")):
-                atoms.append(line.strip())
-                atoms_type.append(line[12:16].strip())
-
-    [print(a) for a in atoms]
-    prot = Molecule("insulin", atoms, n_points)
-    print(prot)
-    print(set(atoms_type))
-    print(prot.neighbor_table)
-
-    inaccessible_points = prot.count_inaccessible_points()
-    accessible_surface = prot.get_accessible_surface()
-    print(f"Number of atoms: {len(prot.atoms)}")
-    print(f"Number of inaccessible points: {inaccessible_points}")
-    print(f"{100 * inaccessible_points / (len(prot.atoms) * n_points)}% of points are inaccessible")
-    print(accessible_surface)
-    print(prot.get_max_surface())
-
-    with open("res.txt", 'w') as f:
-        for atom in prot.atoms:
-            f.write(f"{atom.atom_num} / {atom.atom_name} / {100 * len(atom.accessible_points_list) / n_points}%\n")
+        with open(file, "r") as f:
+            for line in f:
+                if line.startswith(("ATOM")):
+                    atoms.append(line.strip())
+                    atoms_type.append(line[12:16].strip())
     
-    points = []
-    for atom in prot.atoms:
-        points.extend(atom.accessible_points_list)
+        molecule = Molecule(file.stem, atoms, n_points)
+        inaccessible_points = molecule.count_inaccessible_points()
+        accessible_surface = molecule.get_accessible_surface()
+        
+        print(f"Number of atoms: {len(molecule.atoms)}")
+        print(f"Number of accessible points: {len(molecule.atoms) * n_points - inaccessible_points}")
+        print(f"Number of inaccessible points: {inaccessible_points}")
+        print(f"{round(100 * inaccessible_points / (len(molecule.atoms) * n_points), 2)}% of points are inaccessible")
+        print(f"Accessible surface: {accessible_surface} Å2")
+        print(f"Max surface: {round(molecule.get_max_surface(), 2)}Å2")
+    
+        with open(f"{args.get("output_folder")}/{file.stem}.txt", 'w') as f:
+            for atom in molecule.atoms:
+                f.write(f"{atom.atom_num} / {atom.atom_name} / {100 * len(atom.accessible_points_list) / n_points}%\n")
 
-    pts = Points(
-        points,
-        r=10,
-        c="red"
-    )
+    
+    # n_points = 92
+    # pdb_file_test1 = Path("data/pdb/test.pdb")
+    # pdb_file_test2 = Path("data/pdb/benzene.pdb")
+    # pdb_file1 = Path("data/pdb/insulin_3I40.pdb")
+    # pdb_file2 = Path("data/pdb/lysozyme_253L.pdb")
+    # pdb_file3 = Path("data/pdb/lysozyme_1GWD.pdb")
+    # pdb_file4 = Path("data/pdb/1MH1.pdb")
 
-    spheres = [Sphere(
-        pos=atom.coords,
-        r=atom.radius + Atom.vdw_radius.get("water")
-    ).alpha(0.15).c("black" if (atom.atom_name[0] == "C") 
-                            else ("blue" if (atom.atom_name[0] == "N") 
-                                  else ("red" if (atom.atom_name[0] == "O") else "yellow"))) for atom in prot.atoms]
+    # atoms = []
+    # atoms_type = []
 
-    spheres = [Sphere(
-            pos=atom.coords,
-            r=atom.radius + Atom.vdw_radius.get("water")
-        ).alpha(0.15).c("grey") for atom in prot.atoms]
+    # with open(pdb_file2, "r") as f:
+    #     for line in f:
+    #         if line.startswith(("ATOM")):
+    #             atoms.append(line.strip())
+    #             atoms_type.append(line[12:16].strip())
+
+    # [print(a) for a in atoms]
+    # prot = Molecule("insulin", atoms, n_points)
+    # print(prot)
+    # print(set(atoms_type))
+    # print(prot.neighbor_table)
+
+    # inaccessible_points = prot.count_inaccessible_points()
+    # accessible_surface = prot.get_accessible_surface()
+    # print(f"Number of atoms: {len(prot.atoms)}")
+    # print(f"Number of inaccessible points: {inaccessible_points}")
+    # print(f"{100 * inaccessible_points / (len(prot.atoms) * n_points)}% of points are inaccessible")
+    # print(accessible_surface)
+    # print(prot.get_max_surface())
+
+    # with open("res.txt", 'w') as f:
+    #     for atom in prot.atoms:
+    #         f.write(f"{atom.atom_num} / {atom.atom_name} / {100 * len(atom.accessible_points_list) / n_points}%\n")
+    
+    # points = []
+    # for atom in prot.atoms:
+    #     points.extend(atom.accessible_points_list)
+
+    # pts = Points(
+    #     points,
+    #     r=10,
+    #     c="red"
+    # )
+
+    # spheres = [Sphere(
+    #     pos=atom.coords,
+    #     r=atom.radius + Atom.vdw_radius.get("water")
+    # ).alpha(0.15).c("black" if (atom.atom_name[0] == "C") 
+    #                         else ("blue" if (atom.atom_name[0] == "N") 
+    #                               else ("red" if (atom.atom_name[0] == "O") else "yellow"))) for atom in prot.atoms]
+
+    # spheres = [Sphere(
+    #         pos=atom.coords,
+    #         r=atom.radius + Atom.vdw_radius.get("water")
+    #     ).alpha(0.15).c("grey") for atom in prot.atoms]
     
 
-    axes = Axes(
-        xtitle="X",
-        ytitle="Y",
-        ztitle="Z"
-    )
+    # axes = Axes(
+    #     xtitle="X",
+    #     ytitle="Y",
+    #     ztitle="Z"
+    # )
 
-    plotter = Plotter(
-        axes=axes,
-        bg="white"
-    )
+    # plotter = Plotter(
+    #     axes=axes,
+    #     bg="white"
+    # )
 
-    plotter.show(
-        spheres,
-        pts,
-        axes = 1,
-        viewup="z",
-        interactive=True
-    )
+    # plotter.show(
+    #     spheres,
+    #     pts,
+    #     axes = 1,
+    #     viewup="z",
+    #     interactive=True
+    # )
