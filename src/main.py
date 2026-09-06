958223d991a21f8c05bcb9f195d9e89f9f21f731
Mike <fu.mike.pro@gmail.com>
2026-09-06 11:09:40 +0200

    removed block comment

--- src/main.py
+++ src/main.py
@@ -84,11 +84,7 @@ if __name__ == "__main__":
 
         # Export results
         molecule.export_surface_results(output_folder)
-        # with open(f"{output_folder}/{file.stem}.txt", 'w') as f:
-        #     for residue in molecule.residues:
-        #         for atom in residue.atoms:
-        #             f.write(f"{atom.atom_num} / {atom.atom_name} / {100 * len(atom.accessible_points_list) / n_points} %\n")
-
+        
         # Display only the first molecule if display is True
         if display:
             display = False
