7c758a3927dc9e0ba810eb498bfae36a1f85d27e
Mike <921904377x@gmail.com>
2026-09-08 18:29:00 +0200

    begin analysis script

---
+++ src/analysis.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from pathlib import Path
+
+
+with open(Path("data/pdb_ids.txt"), "r") as f:
+    ids = [int(line.strip()) for line in f.readlines()]
+print(ids)
+# results = Path("output/results")
+# naccess_results = Path("output/naccess_results")
+
+# resultats = []
+# for sous_dossier in results.iterdir():
+#     if not sous_dossier.is_dir():
+#         continue
+
+#     identifiant = sous_dossier.name
+
+#     # Calcul de la surface
+#     surface = calculer_surface(sous_dossier)
+
+#     resultats.append({
+#         "identifiant": identifiant,
+#         "surface": surface
+#     })
+
+# df = pd.DataFrame(resultats)
+
+# print(df)
