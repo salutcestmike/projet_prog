29722bb18247ef501395887f84209f6c100e3e47
Mike <921904377x@gmail.com>
2026-09-09 17:39:39 +0200

    analysis

--- src/analysis_glob.py
+++ src/analysis_glob.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import spearmanr, pearsonr
 
 
 if __name__ == "__main__":
     results = Path("output/results/20260908103650")
     naccess_results = Path("output/naccess_results")
+    plt.rcParams.update({'font.size': 16})
 
     with open(Path("data/pdb_ids.txt"), "r") as f:
         ids = [line.strip() for line in f.readlines()]
@@ -17,10 +19,10 @@ if __name__ == "__main__":
 
     for identifiant in ids:
         with open(results / Path(f"{identifiant}/{identifiant}_rsa.txt"), "r") as f:
-            resultats["custom"][identifiant] = float(f.readlines()[-1][6:])
+            resultats["custom"][identifiant] = float(f.readlines()[-1][5:])
 
         with open(naccess_results / Path(f"{identifiant}/.rsa"), "r") as f:
-            resultats["naccess"][identifiant] = float(f.readlines()[-1][6:22])
+            resultats["naccess"][identifiant] = float(f.readlines()[-1][5:23])
 
     print(resultats)
 
@@ -70,3 +72,19 @@ if __name__ == "__main__":
     plt.legend()
 
     plt.show()
+
+
+    spearman, p_spearman = spearmanr(
+        list(resultats["naccess"].values()),
+        list(resultats["custom"].values())
+    )
+
+    pearson, p_pearson = pearsonr(
+        list(resultats["naccess"].values()),
+        list(resultats["custom"].values())
+    )
+
+    print(f"Spearman : {spearman:.4f}")
+    print(f"Spearman p-value: {p_spearman:.4f}")
+    print(f"Pearson  : {pearson:.4f}")
+    print(f"Pearson p-value: {p_pearson:.4f}")
--- src/analysis_prot.py
+++ src/analysis_prot.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import spearmanr
+from scipy.stats import spearmanr, pearsonr
 
 
 if __name__ == "__main__":
-    molecule = "3I40"
+    molecule = "168L"
+    name = "Lysozyme"
     results = Path(f"output/results_test/{molecule}")
     naccess_results = Path(f"output/naccess_results/{molecule}")
-
+    plt.rcParams.update({'font.size': 16})
 
     resultats = {
         "custom" : {},
@@ -25,7 +26,6 @@ if __name__ == "__main__":
             if line.startswith("RES"):
                 resultats["naccess"][int(line[9:14])] = float(line[14:23])
 
-    print(resultats)
 
     plt.figure(figsize=(7, 7))
 
@@ -40,7 +40,7 @@ if __name__ == "__main__":
 
     plt.xlabel("Surface NACCESS")
     plt.ylabel("Surface calculée")
-    plt.title("Comparaison des surfaces")
+    plt.title(f"Comparaison des surfaces, {name} {molecule}")
 
     plt.xlim(lims)
     plt.ylim(lims)
@@ -58,24 +58,56 @@ if __name__ == "__main__":
 
     biais = difference.mean()
     sd = difference.std()
-
+    print(sd)
     plt.figure(figsize=(8, 5))
 
     plt.scatter(moyenne, difference)
 
     plt.axhline(biais, linestyle="--", label=f"Biais = {biais:.2f}")
-    plt.axhline(biais + 1.96 * sd, linestyle=":", label="± 1.96 SD")
+    plt.axhline(biais + 1.96 * sd, linestyle=":", label=f"± {1.96 * sd} SD")
     plt.axhline(biais - 1.96 * sd, linestyle=":")
 
     plt.xlabel("Moyenne des deux méthodes")
     plt.ylabel("Différence (code − NACCESS)")
-    plt.title("Bland–Altman : comparaison des surfaces")
+    plt.title(f"Bland–Altman : comparaison des surfaces, {name} {molecule}")
     plt.legend()
 
     plt.show()
 
     plt.figure(figsize=(8, 5))
-    plt.plot(resultats["custom"].keys(), resultats["custom"].values(), label = "Custom")
-    plt.plot(resultats["naccess"].keys(), resultats["naccess"].values(), label = "Naccess")
-    plt.xticks([1] + list(range(5, max(resultats["naccess"].keys()) + 1, 5)))
+    # plt.plot(resultats["custom"].keys(), resultats["custom"].values(), label = "Custom")
+    # plt.plot(resultats["naccess"].keys(), resultats["naccess"].values(), label = "Naccess")
+    plt.fill_between(
+        list(resultats["custom"].keys()),
+        list(resultats["custom"].values()),
+        label="Surface Custom",
+        alpha = 0.4
+    )
+
+    plt.fill_between(
+            list(resultats["custom"].keys()),
+            list(resultats["naccess"].values()),
+            label="Surface Naccess",
+        alpha = 0.4
+        )
+    plt.legend()
+    plt.xticks([1] + list(range(25, max(resultats["naccess"].keys()) + 1, 25)))
+    plt.title(f"Comparaison des surfaces par résidus, {name} {molecule}")
+    plt.xlabel("Numéro de résidu")
+    plt.ylabel("Surface")
     plt.show()
+
+    spearman, p_spearman = spearmanr(
+        list(resultats["naccess"].values()),
+        list(resultats["custom"].values())
+    )
+
+    pearson, p_pearson = pearsonr(
+        list(resultats["naccess"].values()),
+        list(resultats["custom"].values())
+    )
+
+    print(f"Spearman : {spearman:.4f}")
+    print(f"Spearman p-value: {p_spearman:.4f}")
+    print(f"Pearson  : {pearson:.4f}")
+    print(f"Pearson p-value: {p_pearson:.4f}")
